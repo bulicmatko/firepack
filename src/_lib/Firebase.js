a0c2393802940f2061432d613e2ed6ddc8269743
Matko Bulić <bulicmatko@gmail.com>
2015-11-26 20:58:42 +0100

    test

--- src/_lib/Firebase.js
+++ src/_lib/Firebase.js
@@ -13,6 +13,9 @@ import Config from '../Config';
  *  Firebase
  */
 
+console.log(Config.firebaseUrl);
+setTimeout(() => console.log(Config.firebaseUrl), 1000);
+
 window.Firebase = new Firebase(Config.firebaseUrl);
 
 // Export Firebase
