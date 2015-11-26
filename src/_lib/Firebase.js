b430faed25602161d4c14f3260a1004aa5c2adbd
Matko Bulić <bulicmatko@gmail.com>
2015-11-26 21:18:53 +0100

    testing

--- src/_lib/Firebase.js
+++ src/_lib/Firebase.js
@@ -13,5 +13,7 @@ import Firebase from 'firebase';
 
 window.Firebase = new Firebase('https://fifa-tracker.firebaseio.com');
 
+console.log('Test');
+
 // Export Firebase
 export default new Firebase('https://fifa-tracker.firebaseio.com');
